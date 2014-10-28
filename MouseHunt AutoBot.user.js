d1ef838bbe9f98e8ef283faf811b030171d8c5a6
Harrison Chow <iamnobodyrandom@yahoo.com>
2014-10-28 13:03:58 -0400

    try span

--- MouseHunt AutoBot.user.js
+++ MouseHunt AutoBot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        MouseHunt AutoBot
 // @author      nobodyrandom
-// @version    	1.295
+// @version    	1.296
 // @description An advance user script to automate sounding the hunter horn in MouseHunt application in Facebook with MouseHunt version 3.0 (Longtail) supported and many other features. REVAMPED VERSION of ORIGINAL
 // @require     https://greasyfork.org/scripts/6094-mousehunt-autobot-additional-thing/code/MouseHunt%20AutoBot%20Additional%20thing.js?version=22954
 // @namespace   https://greasyfork.org/users/6398
@@ -1259,7 +1259,7 @@ function embedTimer(targetPage)
                     loadLinkToUpdate.addEventListener ('click', NOBscript, false);
                     
                     text = ' &#126; <a href="https://docs.google.com/spreadsheet/ccc?key=0Ag_KH_nuVUjbdGtldjJkWUJ4V1ZpUDVwd1FVM0RTM1E#gid=5">Click to go to GDoc</a>';
-                    var tempDiv = document.createElement ('div');
+                    var tempDiv = document.createElement ('span');
                     tempDiv.innerHTML = text;
                     loadLinkToUpdateDiv.appendChild (tempDiv);
                     
