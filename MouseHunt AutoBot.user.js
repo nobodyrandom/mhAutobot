a1f85fdefbd68e960511c24a63d70ed62cfc6ced
Harrison Chow <iamnobodyrandom@yahoo.com>
2014-10-28 13:02:32 -0400

    still bugged =,=

--- MouseHunt AutoBot.user.js
+++ MouseHunt AutoBot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        MouseHunt AutoBot
 // @author      nobodyrandom
-// @version    	1.294
+// @version    	1.295
 // @description An advance user script to automate sounding the hunter horn in MouseHunt application in Facebook with MouseHunt version 3.0 (Longtail) supported and many other features. REVAMPED VERSION of ORIGINAL
 // @require     https://greasyfork.org/scripts/6094-mousehunt-autobot-additional-thing/code/MouseHunt%20AutoBot%20Additional%20thing.js?version=22954
 // @namespace   https://greasyfork.org/users/6398
@@ -1258,10 +1258,13 @@ function embedTimer(targetPage)
                     timerDivElement.appendChild (loadLinkToUpdateDiv);
                     loadLinkToUpdate.addEventListener ('click', NOBscript, false);
                     
-                    text = document.createTextNode(' &#126; <a href="https://docs.google.com/spreadsheet/ccc?key=0Ag_KH_nuVUjbdGtldjJkWUJ4V1ZpUDVwd1FVM0RTM1E#gid=5">Click to go to GDoc</a>');
-                    loadLinkToUpdateDiv.appendChild (text);
+                    text = ' &#126; <a href="https://docs.google.com/spreadsheet/ccc?key=0Ag_KH_nuVUjbdGtldjJkWUJ4V1ZpUDVwd1FVM0RTM1E#gid=5">Click to go to GDoc</a>';
+                    var tempDiv = document.createElement ('div');
+                    tempDiv.innerHTML = text;
+                    loadLinkToUpdateDiv.appendChild (tempDiv);
                     
                     text = null;
+                    tempDiv = null;
                     loadLinkToUpdateDiv = null;
                     timersElementToggle = null;
                     loadTimersElement = null;
