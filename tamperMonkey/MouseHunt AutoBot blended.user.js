3f727ec6ec69f7bea8e0439592af3da4f375e00c
Harrison Chow <iamnobodyrandom@yahoo.com>
2014-11-20 18:45:22 -0500

    ver

--- tamperMonkey/MouseHunt AutoBot blended.user.js
+++ tamperMonkey/MouseHunt AutoBot blended.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        MouseHunt AutoBot ENHANCED + REVAMP
 // @author      NobodyRandom, Ooi Keng Siang, CnN
-// @version    	1.4.224b
+// @version    	1.4.225b
 // @description An advance user script to automate sounding the hunter horn in MouseHunt application in Facebook with MouseHunt version 3.0 (Longtail) supported and many other features. REVAMPED VERSION of ORIGINAL by Ooi + ENHANCED VERSION by CnN
 // @require     https://greasyfork.org/scripts/6094-mousehunt-autobot-additional-thing/code/MouseHunt%20AutoBot%20Additional%20thing.js?version=25306
 // @namespace   https://greasyfork.org/users/6398, http://ooiks.com/blog/mousehunt-autobot, https://devcnn.wordpress.com/
@@ -109,7 +109,7 @@ var spongeCharm = ['Double Sponge', 'Sponge'];
 // WARNING - Do not modify the code below unless you know how to read and write the script.
 
 // All global variable declaration and default value
-var scriptVersion = "1.4.222b";
+var scriptVersion = "1.4.225b [revamp + enhance]";
 var fbPlatform = false;
 var hiFivePlatform = false;
 var mhPlatform = false;
