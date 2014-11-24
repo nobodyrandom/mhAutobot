63a5c17eebe2c2956d846d791730967e95ca81c2
Harrison Chow <iamnobodyrandom@yahoo.com>
2014-11-24 12:25:14 -0500

    Finalized chrome package

--- chromeExtension/MouseHunt AutoBot chrome injector.js
+++ chromeExtension/MouseHunt AutoBot chrome injector.js
@@ -1,11 +1,10 @@
 function inject(script) {
-	var s = document.createElement('script');
-// TODO: add "script.js" to web_accessible_resources in manifest.json
-s.src = chrome.extension.getURL(script);
-s.onload = function() {
-    this.parentNode.removeChild(this);
-};
-(document.head||document.documentElement).appendChild(s);
+    var s = document.createElement('script');
+    s.src = chrome.extension.getURL(script);
+    s.onload = function() {
+        this.parentNode.removeChild(this);
+    };
+    (document.head || document.documentElement).appendChild(s);
 }
 inject('MouseHunt AutoBot Additional thing.user.js');
 inject('MouseHunt AutoBot.user.js');
