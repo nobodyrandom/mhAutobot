31d8f2a09d1847f97a7a3fdf9b2da9b87c50ed10
Harrison Chow <iamnobodyrandom@yahoo.com>
2014-11-25 12:46:48 -0500

    ver

--- tamperMonkey/MouseHunt AutoBot Additional thing.user.js
+++ tamperMonkey/MouseHunt AutoBot Additional thing.user.js
@@ -2,7 +2,7 @@
 // @name        MouseHunt AutoBot Additional thing development ver
 // @author      nobodyrandom
 // @namespace   https://greasyfork.org/users/6398
-// @version    	1.1.147d
+// @version    	1.1.148d
 // @license 	GNU GPL v2.0
 // @include		http://mousehuntgame.com/*
 // @include		https://mousehuntgame.com/*
