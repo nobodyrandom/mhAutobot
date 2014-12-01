8cf60da4c8469ed9e45e461b70998440cd277605
Harrison Chow <iamnobodyrandom@yahoo.com>
2014-12-01 17:19:03 -0500

    temp fix =,=

--- tamperMonkey/MouseHunt AutoBot.user.js
+++ tamperMonkey/MouseHunt AutoBot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        MouseHunt AutoBot REVAMP
 // @author      NobodyRandom
-// @version    	1.4.207a
+// @version    	1.4.208a
 // @description An advance user script to automate sounding the hunter horn in MouseHunt application in Facebook with MouseHunt version 3.0 (Longtail) supported and many other features. REVAMPED VERSION of ORIGINAL by Ooi
 // @require		https://greasyfork.org/scripts/6094-mousehunt-autobot-additional-thing/code/MouseHunt%20AutoBot%20Additional%20thing.js?version=26635
 // @namespace   https://greasyfork.org/users/6398
