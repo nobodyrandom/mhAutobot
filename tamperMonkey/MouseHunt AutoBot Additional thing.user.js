aa92d7804cac99f82610fb4521a549ae660865bb
Harrison Chow <iamnobodyrandom@yahoo.com>
2015-01-08 11:59:54 -0500

    ver

--- tamperMonkey/MouseHunt AutoBot Additional thing.user.js
+++ tamperMonkey/MouseHunt AutoBot Additional thing.user.js
@@ -15,7 +15,7 @@
 // ==/UserScript==
 
 // SETTING BASE VARS *******************************
-unsafeWindow.addonScriptVer = '1.1.215';
+unsafeWindow.addonScriptVer = '1.1.216';
 var NOBhasPuzzle = user.has_puzzle;
 var NOBclockLoaded = false;
 var NOBpage = false;
