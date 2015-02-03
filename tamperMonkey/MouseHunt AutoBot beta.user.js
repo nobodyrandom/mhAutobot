91c5ecd82baf78429ce4f115581e04c4447861e2
Harrison Chow <iamnobodyrandom@yahoo.com>
2015-02-03 13:16:14 -0500

    fix grant

--- tamperMonkey/MouseHunt AutoBot beta.user.js
+++ tamperMonkey/MouseHunt AutoBot beta.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        MouseHunt AutoBot REVAMP for BETA UI
 // @author      NobodyRandom
-// @version    	1.5.021y
+// @version    	1.5.022y
 // @description BETA MOUSEHUNT AUTOBOT
 // @require 	https://greasyfork.org/scripts/7601-parse-db-min/code/Parse%20DB%20min.js?version=32976
 // @require		https://greasyfork.org/scripts/7866-mousehunt-autobot-additional-thing-beta/code/MouseHunt%20AutoBot%20Additional%20thing%20BETA.js?version=35066
@@ -17,7 +17,7 @@
 // @include		https://apps.facebook.com/mousehunt/*
 // @include		http://hi5.com/friend/games/MouseHunt*
 // @include		http://mousehunt.hi5.hitgrab.com/*
-// @grant		none
+// @grant		unsafeWindow
 // ==/UserScript==
 
 // == Basic User Preference Setting (Begin) ==
