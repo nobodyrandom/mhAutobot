b7099a76fb26f7f3e77e768d597f150feb213203
Harrison Chow <iamnobodyrandom@yahoo.com>
2015-02-03 12:22:16 -0500

    try add event listener to combat ajax

--- tamperMonkey/MouseHunt AutoBot beta.user.js
+++ tamperMonkey/MouseHunt AutoBot beta.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        MouseHunt AutoBot REVAMP for BETA UI
 // @author      NobodyRandom
-// @version    	1.5.012y
+// @version    	1.5.013y
 // @description BETA AUTOBOT
 // @require 	https://greasyfork.org/scripts/7601-parse-db-min/code/Parse%20DB%20min.js?version=32976
 // @require		https://greasyfork.org/scripts/7866-mousehunt-autobot-additional-thing-beta/code/MouseHunt%20AutoBot%20Additional%20thing%20BETA.js?version=34950
@@ -1062,6 +1062,8 @@ function embedTimer(targetPage) {
             	// try check if ajax was called
             	if (doubleCheckLocation()) {
             		exeScript();
+            	} else {
+            		$('.camp a')[0].addEventListener('click', exeScript);
             	}
             	
                 // player currently navigating other page instead of hunter camp
