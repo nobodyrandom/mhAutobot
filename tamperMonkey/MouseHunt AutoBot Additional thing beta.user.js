0b26c62899d4d002a9133fd152125cfbfee05b76
Harrison Chow <iamnobodyrandom@yahoo.com>
2015-02-08 17:16:14 -0500

    retime parse logout

--- tamperMonkey/MouseHunt AutoBot Additional thing beta.user.js
+++ tamperMonkey/MouseHunt AutoBot Additional thing beta.user.js
@@ -2,7 +2,7 @@
 // @name        MouseHunt AutoBot Additional thing BETA
 // @author      NobodyRandom
 // @namespace   https://greasyfork.org/users/6398
-// @version    	1.3.019z
+// @version    	1.3.020z
 // @description	This is an additional file for NobodyRandom's version of MH autobot (https://greasyfork.org/en/scripts/6092-mousehunt-autobot-revamp) BETA
 // @license 	GNU GPL v2.0
 // @include		http://mousehuntgame.com/*
@@ -16,7 +16,7 @@
 // ==/UserScript==
 
 // SETTING BASE VARS *******************************
-unsafeWindow.addonScriptVer = '1.3.019z';
+unsafeWindow.addonScriptVer = '1.3.020z';
 var NOBhasPuzzle = user.has_puzzle;
 var NOBclockLoaded = false;
 var NOBpage = false;
@@ -430,8 +430,8 @@ function pingServer() {
                     console.log("Parse failed - " + error);
                 }
             });
-
-            Parse.User.logOut();
+        }).then(function() {
+        	Parse.User.logOut();
         });
     }
 }
