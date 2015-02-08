9092b972e70a7a71f71a18c5d3b4dc7f190fe2bd
Harrison Chow <iamnobodyrandom@yahoo.com>
2015-02-08 14:29:15 -0500

    activate ping again

--- tamperMonkey/MouseHunt AutoBot Additional thing beta.user.js
+++ tamperMonkey/MouseHunt AutoBot Additional thing beta.user.js
@@ -2,7 +2,7 @@
 // @name        MouseHunt AutoBot Additional thing BETA
 // @author      NobodyRandom
 // @namespace   https://greasyfork.org/users/6398
-// @version    	1.3.010z
+// @version    	1.3.011z
 // @description	This is an additional file for NobodyRandom's version of MH autobot (https://greasyfork.org/en/scripts/6092-mousehunt-autobot-revamp) BETA
 // @license 	GNU GPL v2.0
 // @include		http://mousehuntgame.com/*
@@ -16,7 +16,7 @@
 // ==/UserScript==
 
 // SETTING BASE VARS *******************************
-unsafeWindow.addonScriptVer = '1.3.010z';
+unsafeWindow.addonScriptVer = '1.3.011z';
 var NOBhasPuzzle = user.has_puzzle;
 var NOBclockLoaded = false;
 var NOBpage = false;
@@ -86,7 +86,7 @@ function NOBinit() {
             clockTick();
             fetchGDocStuff();
             setTimeout(function() {
-                //pingServer();
+                pingServer();
             }, 30000);
         }
     }
