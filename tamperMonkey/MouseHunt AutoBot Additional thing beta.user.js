a01ea14f3a5cc0a46bf5508ff8a9a885cdbbef77
Harrison Chow <iamnobodyrandom@yahoo.com>
2015-02-09 14:19:52 -0500

    ver

--- tamperMonkey/MouseHunt AutoBot Additional thing beta.user.js
+++ tamperMonkey/MouseHunt AutoBot Additional thing beta.user.js
@@ -2,7 +2,7 @@
 // @name        MouseHunt AutoBot Additional thing BETA
 // @author      NobodyRandom
 // @namespace   https://greasyfork.org/users/6398
-// @version    	1.3.028z
+// @version    	1.3.029z
 // @description	This is an additional file for NobodyRandom's version of MH autobot (https://greasyfork.org/en/scripts/6092-mousehunt-autobot-revamp) BETA
 // @license 	GNU GPL v2.0
 // @include		http://mousehuntgame.com/*
@@ -16,7 +16,7 @@
 // ==/UserScript==
 
 // SETTING BASE VARS *******************************
-unsafeWindow.addonScriptVer = '1.3.027z';
+unsafeWindow.addonScriptVer = '1.3.029z';
 var NOBhasPuzzle = user.has_puzzle;
 var NOBclockLoaded = false;
 var NOBpage = false;
