6230d105d63310722ec99b0646cecf9c6dbb1e42
Harrison Chow <iamnobodyrandom@yahoo.com>
2015-03-24 12:29:03 -0400

    ver

--- tamperMonkey/MouseHunt AutoBot beta.user.js
+++ tamperMonkey/MouseHunt AutoBot beta.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name        MouseHunt AutoBot REVAMP for BETA UI
 // @author      NobodyRandom
-// @version    	1.5.065y
+// @version    	1.5.066y
 // @description BETA MOUSEHUNT AUTOBOT for the BETA MH UI
 // @require 	https://greasyfork.org/scripts/7601-parse-db-min/code/Parse%20DB%20min.js?version=32976
-// @require		https://greasyfork.org/scripts/7866-mousehunt-autobot-additional-thing-beta/code/MouseHunt%20AutoBot%20Additional%20thing%20BETA.js?version=41364
+// @require		https://greasyfork.org/scripts/7866-mousehunt-autobot-additional-thing-beta/code/MouseHunt%20AutoBot%20Additional%20thing%20BETA.js?version=42538
 // @namespace   https://greasyfork.org/users/6398
 // @updateURL	https://greasyfork.org/scripts/7865-mousehunt-autobot-revamp-for-beta-ui/code/MouseHunt%20AutoBot%20REVAMP%20for%20BETA%20UI.user.js
 // @downloadURL	https://greasyfork.org/scripts/7865-mousehunt-autobot-revamp-for-beta-ui/code/MouseHunt%20AutoBot%20REVAMP%20for%20BETA%20UI.user.js
