3ea1486ee95300fc29357ea0fb6b4bf5e40eb65c
Harrison Chow <iamnobodyrandom@yahoo.com>
2015-04-14 13:08:21 -0400

    ver

--- tamperMonkey/MouseHunt AutoBot blended.user.js
+++ tamperMonkey/MouseHunt AutoBot blended.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        MouseHunt AutoBot ENHANCED + REVAMP
 // @author      NobodyRandom, Ooi Keng Siang, CnN
-// @version    	1.4.541b
+// @version    	1.4.542b
 // @description Currently the most advanced script for automizing MouseHunt. Supports ALL new areas. REVAMPED VERSION of ORIGINAL by Ooi + ENHANCED VERSION by CnN - Beta UI version: https://greasyfork.org/en/scripts/7865-mousehunt-autobot-revamp-for-beta-ui
 // @require		https://greasyfork.org/scripts/7601-parse-db-min/code/Parse%20DB%20min.js?version=32976
 // @require     https://greasyfork.org/scripts/6094-mousehunt-autobot-additional-thing/code/MouseHunt%20AutoBot%20Additional%20thing.js?version=41363
@@ -694,7 +694,7 @@ function gnawnianExpress(load) {
 				var attacking = document.getElementsByClassName('attacked');
 				for (var i = 0; i < attacking.length; i++) {
 					if (attacking[i].tagName == 'DIV')
-						attacking = attacking[i].className.substr(0, attacking.className.indexOf(' '));;
+						attacking = attacking[i].className.substr(0, attacking[i].className.indexOf(' '));;
 				}
 				console.debug("Raiders are attacking " + attacking);
 				switch (attacking) {
--- tamperMonkey/MouseHunt AutoBot.user.js
+++ tamperMonkey/MouseHunt AutoBot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        MouseHunt AutoBot REVAMP
 // @author      NobodyRandom
-// @version    	1.4.541a
+// @version    	1.4.542a
 // @description Currently the most advanced script for automizing MouseHunt. Supports ALL new areas. REVAMPED VERSION of ORIGINAL by Ooi - Beta UI version: https://greasyfork.org/en/scripts/7865-mousehunt-autobot-revamp-for-beta-ui
 // @require		https://greasyfork.org/scripts/7601-parse-db-min/code/Parse%20DB%20min.js?version=32976
 // @require		https://greasyfork.org/scripts/6094-mousehunt-autobot-additional-thing/code/MouseHunt%20AutoBot%20Additional%20thing.js?version=41363
