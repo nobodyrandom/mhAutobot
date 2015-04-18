54bfee37d429921118c5ca9d807104f22f081960
Harrison Chow <iamnobodyrandom@yahoo.com>
2015-04-18 14:14:55 -0400

    test

--- tamperMonkey/MouseHunt AutoBot blended.user.js
+++ tamperMonkey/MouseHunt AutoBot blended.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        MouseHunt AutoBot ENHANCED + REVAMP
 // @author      NobodyRandom, Ooi Keng Siang, CnN
-// @version    	1.4.550b
+// @version    	1.4.551b
 // @description Currently the most advanced script for automizing MouseHunt. Supports ALL new areas. REVAMPED VERSION of ORIGINAL by Ooi + ENHANCED VERSION by CnN - Beta UI version: https://greasyfork.org/en/scripts/7865-mousehunt-autobot-revamp-for-beta-ui
 // @require		https://greasyfork.org/scripts/7601-parse-db-min/code/Parse%20DB%20min.js?version=32976
 // @require     https://greasyfork.org/scripts/6094-mousehunt-autobot-additional-thing/code/MouseHunt%20AutoBot%20Additional%20thing.js?version=46706
@@ -841,25 +841,28 @@ function checkThenArm(sort, category, name) //category = weapon/base/charm/trink
 			var theCharmArmed = document.getElementById('hud_trapPower').innerText;
 			if (sort == 'best') {
 				for (var i = 0; i < name.length; i++) {
+					console.log(theCharmArmed + " + " + name[i]);
+					
 					if (name[i].length > 13) {
 						name[i] = name[i].substring(0, 13);
 						name[i] += "...";
 					}
-					
+										
 					if (theCharmArmed == name[i]) {
 						trapArmed = true;
 						break;
 					}
 				}
 			} else {
-				if (name[i].length > 13) {
-						name[i] = name[i].substring(0, 13);
-						name[i] += "...";
-					}
-					
-					if (theCharmArmed == name[i]) {
-						trapArmed = true;
-					}
+				console.log(theCharmArmed + " + " + name);
+				if (name.length > 13) {
+					name = name.substring(0, 13);
+					name += "...";
+				}
+				
+				if (theCharmArmed == name[i]) {
+					trapArmed = true;
+				}
 			}
 			//trapArmed = true;
 		}
